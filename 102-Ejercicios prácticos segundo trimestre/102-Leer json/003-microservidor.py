07ce91b3ebeb92b6c25b123cb56fda1d92081f8e
danicp22 <daniel.calve.pardo@alu.ceacfp.es>
2025-11-20 12:17:32 +0100

    1

---
+++ 102-Ejercicios prácticos segundo trimestre/102-Leer json/003-microservidor.py
@@ -0,0 +1,16 @@
+# Importo libreria flask para crear web
+from flask import Flask, render_template
+
+# Creo una nueva aplicacion
+app = Flask(__name__)
+
+# Escucho en la ruta raiz
+@app.route("/")
+def inicio():   
+    # Renderizo una plantilla llamada index.html
+    return render_template("index.html")
+
+# Si este archivo no es una libreria y es el archivo principal
+if __name__ == "__main__":
+    # Pon en marcha la aplicacion
+    app.run(debug=True)
