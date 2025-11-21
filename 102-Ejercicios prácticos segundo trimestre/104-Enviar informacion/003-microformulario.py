6b0ee1535e1fae6739f45047cbdb0296f5b408c0
danicp22 <daniel.calve.pardo@alu.ceacfp.es>
2025-11-21 14:18:44 +0100

    1

--- 102-Ejercicios prácticos segundo trimestre/104-Enviar informacion/003-microformulario.py
+++ 102-Ejercicios prácticos segundo trimestre/104-Enviar informacion/003-microformulario.py
@@ -11,7 +11,7 @@ def envio():
     nombre = request.args.get("nombre")
     apellidos = request.args.get("apellidos")
     print(nombre,apellidos)
-    return "Mira en la consola si ha pasado algo" 
+    return "nombre: "+nombre+" - apellidos: "+apellidos 
 
 if __name__ == "__main__":
     app.run(debug=True)
