8b16fb083b57062d7829feaa111d85aba14b4f27
danicp22 <daniel.calve.pardo@alu.ceacfp.es>
2025-11-21 10:46:34 +0100

    1

---
+++ 102-Ejercicios prácticos segundo trimestre/103-Jinja/008-paso tabla bidimensional.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template
+import mysql.connector
+################################## MYSQL ##################################################
+conexion = mysql.connector.connect(   
+    host="localhost",
+    user="dani",
+    password="Dani12345&",
+    database="portafolioexamen"
+)
+
+cursor = conexion.cursor()                                
+# ------------------------------ Esto envia las tablas-------------------------------------
+cursor.execute("SHOW TABLES;")                               
+tablas = []                                                
+filas = cursor.fetchall()                                 
+for fila in filas:                                          
+    tablas.append(fila[0])                                   
+# ----------------------------- Esto envia las cabeceras de las tablas --------------------
+cursor.execute("SHOW COLUMNS in piezasportafolio;")                               
+columnas = []                                                  
+filas = cursor.fetchall()                                    
+for fila in filas:                                            
+    columnas.append(fila[0]) 
+# ------------------------------ Esto envia toda la tabla ----------------------------------
+cursor.execute("SELECT * FROM piezasportafolio;")                               
+contenido_tabla = cursor.fetchall()                                    
+################################## MYSQL ##################################################
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def inicio(): 
+    return render_template(
+        "backoffice.html",
+        mis_tablas = tablas,
+        mis_columnas = columnas,
+        mi_contenido_tabla = contenido_tabla
+        ) 
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
