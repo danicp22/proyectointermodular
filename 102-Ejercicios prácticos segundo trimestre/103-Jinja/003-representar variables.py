8efc722e995996b45b6310cf6cf7f1bfc43b1ddc
danicp22 <daniel.calve.pardo@alu.ceacfp.es>
2025-11-21 09:39:32 +0100

    1

--- 102-Ejercicios prácticos segundo trimestre/103-Jinja/003-representar variables.py
+++ 102-Ejercicios prácticos segundo trimestre/103-Jinja/003-representar variables.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def inicio():   
-    return render_template("variable.html")
+def inicio():
+    # mi_nombre es la variable que esta en python
+    mi_nombre = "Daniel"   
+    # nombre es la variable que lanzo a la pantalla
+    return render_template("variable.html" ,nombre=mi_nombre)
 
 
 if __name__ == "__main__":
---
+++ 102-Ejercicios prácticos segundo trimestre/103-Jinja/004-directiva for.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def inicio():
+    mis_frutas = ["Manzanas","Peras","Platanos","Fresas"]  
+    return render_template("lista.html" ,frutas=mis_frutas)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
