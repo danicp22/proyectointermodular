71bffceba5b2df317718b5e35f44ee5d12ccc4f8
danicp22 <daniel.calve.pardo@alu.ceacfp.es>
2025-11-21 14:15:49 +0100

    1

---
+++ 102-Ejercicios prácticos segundo trimestre/104-Enviar informacion/002-cojo parametro.py
@@ -0,0 +1,12 @@
+from flask import Flask, request  # Tomo parametros de la url
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    nombre = request.args.get("nombre")
+    print(nombre)
+    return "Mira en la consola si ha pasado algo" 
+
+if __name__ == "__main__":
+    app.run(debug=True)
